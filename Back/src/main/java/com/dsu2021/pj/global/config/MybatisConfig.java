726307edbf87be9b5558af2873b5796deeaede43
pjunh1117 <pjunh1117@gmail.com>
2021-08-13 17:58:39 +0900

    mybatis

---
+++ Back/src/main/java/com/dsu2021/pj/global/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package com.dsu2021.pj.global.config;
+
+//mybatis 설정 클래스
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class MybatisConfig {
+}
