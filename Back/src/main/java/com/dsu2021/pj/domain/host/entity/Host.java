5a93e6fa6ef595266b1ff04d6ed02bb21d1b295d
Young37 <20171470@g.dongseo.ac.kr>
2021-08-17 20:20:51 +0900

    호스트 도메인, 엔티티 추가

---
+++ Back/src/main/java/com/dsu2021/pj/domain/host/entity/Host.java
@@ -0,0 +1,13 @@
+package com.dsu2021.pj.domain.host.entity;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Host {
+	private Long user_index;
+	private Date start_date;
+}
