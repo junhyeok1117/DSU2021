47536fa462892dfa3e1f122f820605e735eaba84
pjunh1117 <pjunh1117@gmail.com>
2021-08-18 15:50:11 +0900

    작성 팁

--- Back/src/main/java/com/dsu2021/pj/domain/user/dto/UserDTO.java
+++ Back/src/main/java/com/dsu2021/pj/domain/user/dto/UserDTO.java
@@ -1,6 +1,38 @@
 package com.dsu2021.pj.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class UserDTO{
-	
+
+    /*
+        유저 회원가입 요청 DTO
+     */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserSaveReq {
+
+    }
+
+    /**
+     * 유저 정보 전달 DTO
+     */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserSelectRes {
+        //클라이언트한테 확인차 던지는 값
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserIdRes {
+        private Long id;
+    }
 	
 }
