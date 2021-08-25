f655c11e84fdd4577f5097c2bbb886f09347ea45
pjunh1117 <pjunh1117@gmail.com>
2021-08-25 14:17:31 +0900

    mybatis관련 수정 resources.mapper에 mapper.xml 설정 mybatis-config.xml에서 별칭 지정

--- Back/src/main/java/com/dsu2021/pj/domain/user/entity/User.java
+++ Back/src/main/java/com/dsu2021/pj/domain/user/entity/User.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.apache.ibatis.type.Alias;
 
-@Alias("User")//mybatis에서 패키지 전체 입력을 대신하는 별칭
 @AllArgsConstructor
 @Getter
 public class User {
